443a931ea559708c1fd6f33603170157da15f7d6
mamensu_ <39234389+qqaazz0222@users.noreply.github.com>
2024-10-30 16:59:04 +0900

    Chore: Update API URL

--- src/services/demoService.js
+++ src/services/demoService.js
@@ -1,9 +1,9 @@
 const BaseURL = "http://43.202.202.125";
-const AbnormalApi = BaseURL + "/api/abnormal";
-const QnaApi = BaseURL + "/api/qna";
-const DeepscanApi = BaseURL + "/api/deepscan";
-const SelfharmApi = BaseURL + "/api/selfharm";
-const FalldownApi = BaseURL + "/api/falldown";
+const AbnormalApi = BaseURL + ":50000";
+const QnaApi = BaseURL + ":50001";
+const DeepscanApi = BaseURL + "50002";
+const SelfharmApi = BaseURL + "50003";
+const FalldownApi = BaseURL + "50004";
 const Available = [AbnormalApi, FalldownApi];
 const OnStudy = [QnaApi, DeepscanApi, SelfharmApi];
 
